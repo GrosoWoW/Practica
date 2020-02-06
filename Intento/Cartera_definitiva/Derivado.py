dc07b40a9a5c98a8887e579ea937b04e3d2c76af
GrosoWoW <cristobal.jaramillo@ug.uchile.cl>
2020-02-06 10:25:03 -0300

    modificando derivado

--- Intento/Cartera_definitiva/Derivado.py
+++ Intento/Cartera_definitiva/Derivado.py
@@ -1,14 +1,22 @@
 from Activo import Activo
-
+import datetime
+import pandas as pd
+import numpy as np
+from UtilesValorizacion import parsear_curva
+from Matematica import interpolacion_log_escalar
 
 
 class Derivado(Activo):
 
-    def __init__(self, derivado_generico):
+    def __init__(self, derivado_generico, monedaCartera, fecha_valorizacion, cn):
+
+        super(Derivado, self).__init__(monedaCartera, fecha_valorizacion, cn)
 
         self.derivado_generico = derivado_generico
         self.derivado_generico.genera_flujos()
         self.derivado_generico.valoriza_flujos()
+
+
 
     def get_derivado_generico(self):
 
@@ -17,29 +25,44 @@ class Derivado(Activo):
     def get_flujos(self):
 
         return self.get_derivado_generico().flujos_valorizados[["ID","ActivoPasivo", "Fecha"\
-            , "FechaFixing", "FechaFlujo", "FechaPago", "Flujo", "ValorPresenteMonFlujo", "Moneda", "MonedaBase", "MonedaPasivo"]]
+            , "FechaFixing", "FechaFlujo", "FechaPago", "Flujo", "ValorPresenteMonFlujo", "Moneda", "MonedaBase"]]
+
+    def seleccionar_curva_derivados(self, moneda, n, fecha=datetime.date(2018, 1, 22)):
+
+        monedas = moneda
+        cnn = self.get_cn()
+
+        if moneda == "UF": #Funciona para el error de CLF
+            monedas = "CLF"
+
+        curva = ("SELECT TOP(" + str(n) + ")* FROM [dbDerivados].[dbo].[TdCurvasDerivados] WHERE Tipo = 'CurvaEfectiva_"+ str(monedas) +"' AND Hora = '1500' AND Fecha > '" + str(fecha) + "'")
+        curva = pd.read_sql(curva, cnn)
+        return curva
 
 
     def set_historico(self):
 
         n = 200
-        moneda = self.get_monedaActivo()
-        curvas = seleccionar_curva_derivados(moneda, n)[::-1]
+        moneda = self.get_flujos()["Moneda"][0]
+        curvas = self.seleccionar_curva_derivados(moneda, n)[::-1]
 
         largo = len(self.get_plazos())
         cantidad_curvas = len(curvas["Curva"])
+        pivotes = self.get_plazos()
 
-        matriz = np.zeros(largo, cantidad_curvas)
+        matriz = np.zeros([cantidad_curvas, largo])
 
+        # Por cada plazo
         for i in range(largo):
-
+            
+            # Por cada curva
             for j in range(cantidad_curvas):
 
                 valor_dia = pivotes[i]
                 curva = curvas["Curva"][j]
                 fecha_curva = curvas["Fecha"][j]
                 curva_parseada = parsear_curva(curva, fecha_curva)
-                matriz[i][j] = interpolacion_log_escalar(valor_dia, curva_parseada)
+                matriz[j][i] = interpolacion_log_escalar(valor_dia, curva_parseada)
                 
         self.historicos = pd.DataFrame(matriz)
 
