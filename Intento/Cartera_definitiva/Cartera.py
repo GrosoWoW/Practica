a7af57ce2f49e7b6cc75b7d71be392582e160e3f
SanquirinoB <fernanda.sanchirico@ug.uchile.cl>
2020-02-10 10:01:26 -0300

    Actualizacion

--- Intento/Cartera_definitiva/Cartera.py
+++ Intento/Cartera_definitiva/Cartera.py
@@ -236,6 +236,7 @@ class Cartera:
         :return: DataFrame con la covarianza total de la cartera
 
         """
+        return self.covarianza
 
     def get_volatilidad_cartera(self):
 
@@ -380,7 +381,7 @@ class Cartera:
 
             derivados[i].set_distribucion_pivotes()
 
-    def volatilidad_cartera(self):
+    def set_volatilidad_cartera(self):
 
         vector = self.get_vector_supremo()
         suma = sum(vector)
@@ -462,80 +463,5 @@ class Cartera:
         vector_supremo.extend(derivados.iloc[:,0])
 
         self.vector_supremo = vector_supremo
-
-
-# Conexion
-server = '172.16.1.38'
-username = 'sa'
-password = 'qwerty123'
-driver = '{ODBC Driver 17 for SQL Server}'
-cn = pyodbc.connect('DRIVER=' + driver + ';SERVER=' + server + ';UID=' + username + ';PWD=' + password)
-
-# Acciones
-archivo = pd.read_excel('C:\\Users\\groso\\Desktop\\Practica\\Intento\\Cartera_definitiva\\AMZN.xlsx')    
-#archivo = pd.read_excel('C:\\Users\\Lenovo\\Documents\\Universidad\\Practica\\Cartera_V2\\Practica\\Intento\\Cartera_definitiva\\AMZN.xlsx')
-columnas = ["Date", "Open", "High", "Low", "Close", "Adj Close", "Volume"]
-archivo = archivo[columnas][:200]
-accion = pd.DataFrame()
-accion['Moneda'] = ['USD']
-accion['Historico'] = [[archivo["Adj Close"]]]
-accion['Nombre'] = "Amazon"
-accion['Inversion'] = [500000]
-# Bonos
-bono = pd.DataFrame()
-bono['Riesgo'] = ['AAA']  
-bono['Moneda'] = ["CLP"]
-bono['TablaDesarrollo'] = ["1#25-09-2018#2,2252#0#100#2,2252|2#25-03-2019#2,2252#0#100#2,2252|3#25-09-2019#2,2252#0#100#2,2252|4#25-03-2020#2,2252#0#100#2,2252|5#25-09-2020#2,2252#0#100#2,2252|6#25-03-2021#2,2252#100#0#102,2252"]
-bono['Convencion'] = ["ACT360"]
-bono['FechaEmision'] = ['2018-02-20']
-
-bono2 = pd.DataFrame()
-bono2['Riesgo'] = ['AAA']  
-bono2['Moneda'] = ["CLP"]
-bono2['TablaDesarrollo'] = ["1#25-09-2018#2,2252#0#100#2,2252|2#25-03-2019#2,2252#0#100#2,2252|3#25-09-2019#2,2252#0#100#2,2252|4#25-03-2020#2,2252#0#100#2,2252|5#25-09-2020#2,2252#0#100#2,2252|6#25-03-2021#2,2252#100#0#102,2252"]
-bono2['Convencion'] = ["ACT360"]
-bono2['FechaEmision'] = ['2018-02-20']
-
-bonos = pd.concat([bono, bono2], 0)
-# Derivado
-info_derivado = dict()
-info_derivado["Tipo"] = 'SCC'
-info_derivado["ID_Key"] = ""
-info_derivado["Administradora"] = "Admin"
-info_derivado["Fondo"] = "Fondo"
-info_derivado["Contraparte"] = "Contraparte"
-info_derivado["ID"] = 3
-info_derivado["Nemotecnico"] = ""
-info_derivado["Mercado"] = "Local"
-fecha = datetime.date(2019, 12, 20)
-hora = '1700'
-info_derivado["FechaEfectiva"] = '12/11/2019'
-info_derivado["FechaVenc"] = '12/11/2021'
-info_derivado["AjusteFeriados"] = "CL"
-info_derivado["NocionalActivo"] = 10*(10**9)
-info_derivado["MonedaActivo"] = 'CLP'
-info_derivado["MonedaBase"] = 'CLP'
-info_derivado["TipoTasaActivo"] = 'Fija'
-info_derivado["TipoTasaPasivo"] = 'Flotante'
-info_derivado["TasaActivo"] = 1.45
-info_derivado["TasaPasivo"] = -1000
-info_derivado["FrecuenciaActivo"] = "Semi annual"
-info_derivado["FrecuenciaPasivo"] = info_derivado["FrecuenciaActivo"]
-
-info1 = pd.DataFrame([info_derivado])
-derivado_info = DerivadosSCC(fecha, hora, info1, cn)
-derivado = pd.DataFrame()
-derivado['Derivado'] = [derivado_info]
-
-
-
-
-cartera = Cartera(accion, bonos, derivado, 'CLP', datetime.date(2019,2,1), cn)
-
-
-print(cartera.get_historicos_totales())
-print(cartera.get_retornos_totales())
-print(cartera.get_volatilidades_totales())
-
 
 
